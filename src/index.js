790db045d8a79cbe76f95534b3e33ab89e8ca74f
Mudit Sharma <muditshar001@gmail.com>
2023-11-19 22:11:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
 const fileContent = "Newton School";
+
 const writeFile = async (fileName, fileContent) => {
-    //Write your code here
-    //Don't change function name
-  
+  try {
+    // Check if the file already exists
+    await fs.access(fileName);
+
+    // If the file exists, log a message and skip write operation
+    console.log(`${fileName} already exists. Skipping write operation`);
+  } catch (error) {
+    // If the file does not exist, create the file and write content
+    await fs.writeFile(fileName, fileContent);
+    console.log(`File ${fileName} created and data written successfully!`);
+  }
 };
-module.exports =  writeFile ;
+
+module.exports = writeFile;
+
